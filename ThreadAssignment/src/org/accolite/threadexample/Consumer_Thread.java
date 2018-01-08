aef12882d3978b5d8641dcec8211df084779f85a
rajat batra <rajat.batra@accoliteindia.com>
2018-01-08 16:37:47 +0530

    file changed to consumer_thread

---
+++ ThreadAssignment/src/org/accolite/threadexample/Consumer_Thread.java
@@ -0,0 +1,38 @@
+package org.accolite.threadexample;
+
+public class Consumer_Thread implements Runnable {
+	
+	int fruit;
+	int quantity;
+	ResultStore r;
+	public Consumer_Thread(int fruit,int quantity,ResultStore r)
+	{
+		this.fruit=fruit;
+		this.quantity=quantity;
+		this.r=r;
+	}
+	public void run()
+	{
+		int avail_fruit = r.market.get(r.market.keySet().toArray()[fruit]);
+		System.out.println( " But market have " + avail_fruit + " quantity left ");
+
+		if(avail_fruit<=quantity)
+		{
+			System.out.println("sorry !! consumer has to wait");
+		}
+		else
+		{
+			System.out.println("consumer consume");
+			System.out.println("left fruits in market");
+			System.out.println(r.market);
+			System.out.println();
+			
+			r.market.put((String)r.market.keySet().toArray()[fruit],r.market.get(r.market.keySet().toArray()[fruit])-quantity);
+			
+			
+			r.empty+=quantity;
+			r.full-=quantity;
+		}
+	}
+
+}
