d2312c8e86d776b24e649d77a1edbdb25342f7b7
rajat batra <rajat.batra@accoliteindia.com>
2018-01-08 16:33:03 +0530

    file changed to farmer_thread

---
+++ ThreadAssignment/src/org/accolite/threadexample/Farmer_Thread.java
@@ -0,0 +1,45 @@
+package org.accolite.threadexample;
+import java.util.*;
+public class Farmer_Thread implements Runnable {
+	int quantity;
+	//Integer i;
+	ResultStore r;
+	Scanner sc = new Scanner(System.in);
+	Farmer_Thread(int quantity,ResultStore r)
+	
+	{
+		this.quantity=quantity;
+		this.r=r;
+		
+	}
+	public void run()
+	{
+		if(quantity>(r.empty))
+		{
+			System.out.println("But available market is" + r.empty+", market is full,sorry!!");
+		}
+		else
+		{
+			Random rand = new Random();
+			
+			int fruit_index = rand.nextInt(4); 
+			System.out.print("farmer add "+ (String)r.market.keySet().toArray()[fruit_index]);
+			System.out.println(" " +quantity  +" units");
+			System.out.println("current market : ");
+			r.market.put((String)r.market.keySet().toArray()[fruit_index] ,r.market.get(r.market.keySet().toArray()[fruit_index])+quantity);
+			
+		
+			System.out.println(r.market);
+			//sc.nextLine();
+			
+			r.empty-=quantity;
+			r.full+=quantity;
+		}
+			}
+		}
+		
+	
+	
+	
+
+
